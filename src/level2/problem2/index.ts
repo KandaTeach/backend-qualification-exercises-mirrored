c065fa406400f99d7882cc633533ed238d574b3c
KandaTeach <www.keneth.com.ph@gmail.com>
2024-08-09 16:07:10 +0800

    solved level 2 problem 2

--- src/level2/problem2/index.ts
+++ src/level2/problem2/index.ts
@@ -4,5 +4,26 @@ export function merge(...args: DowntimeLogs[]): DowntimeLogs {
   /**
    * insert your code here
    */
-  return [];
+  const logs: DowntimeLogs = args.flat();
+
+  logs.sort((a, b) => a[0].getTime() - b[0].getTime());
+
+  const mergeLogs: DowntimeLogs = [];
+
+  for (const [start, end] of logs) {
+    if (mergeLogs.length === 0) {
+      mergeLogs.push([start, end]);
+    } else {
+      const last = mergeLogs[mergeLogs.length - 1];
+      if (start <= last[1]) {
+        // Overlapping or contiguous intervals, merge them
+        last[1] = new Date(Math.max(end.getTime(), last[1].getTime()));
+      } else {
+        // No overlap, push a new interval
+        mergeLogs.push([start, end]);
+      }
+    }
+  }
+
+  return mergeLogs;
 }
