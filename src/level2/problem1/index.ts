6fa32a4798f607f900f180e70f9c964e6e776ce4
KandaTeach <www.keneth.com.ph@gmail.com>
2024-08-09 16:01:57 +0800

    solved level 2 problem 1

--- src/level2/problem1/index.ts
+++ src/level2/problem1/index.ts
@@ -1,11 +1,20 @@
 export class ExecutionCache<TInputs extends Array<unknown>, TOutput> {
+
+  private cache: Map<string, Promise<TOutput>> = new Map();
+
   constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) {}
-  
+
   async fire(key: string, ...args: TInputs): Promise<TOutput> {
     /**
      * insert your code here
      */
-  
-    return;
+    if(!this.cache.has(key)) {
+      const result = this.handler(...args);
+      this.cache.set(key, result);
+
+      return result;
+    }
+
+    return this.cache.get(key);
   }
 }
